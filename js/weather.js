5d16385fe46a6eb1781fd3724d9e46f0e5952863
Allison Marie Munn <ammunn.suitesofeuston@gmail.com>
2025-08-08 16:54:25 +0000

    Fixed Bug Fixed Alert Bug.
    MODIFIED: weather.js

--- js/weather.js
+++ js/weather.js
@@ -10,8 +10,6 @@ function weather()
     s.parentNode.insertBefore(script, s);
   }
 
-  alert(process.env.FOO);
-
   window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
   window.myWidgetParam.push( {id: 15,cityid: '5920288',appid: '2b46ac37788852681158f7d632d4c0af',units: 'metric',containerid: 'weather',  });  
   setup();
