c40f519ba4896fd9574c484dfdd06e14bd963d36
Allison Marie Munn <ammunn.suitesofeuston@gmail.com>
2025-08-08 04:35:29 +0000

    Added Weather Added weather to app.
    MODIFIED: app.html
    ADDED: weather.js

---
+++ js/weather.js
@@ -0,0 +1,16 @@
+function weather()
+{
+  function setup()
+  {
+    var script = document.createElement('script');
+    script.async = true;
+    script.charset = "utf-8";
+    script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(script, s);
+  }
+
+  window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
+  window.myWidgetParam.push( {id: 15,cityid: '5920288',appid: '2b46ac37788852681158f7d632d4c0af',units: 'metric',containerid: 'openweathermap-widget-15',  });  
+  setup();
+}
