ae90281e46055c1d68c7261d9367347ac0253681
Allison Marie Munn <ammunn.suitesofeuston@gmail.com>
2025-08-08 15:56:07 +0000

    Added debug and initial code for bike support in app.
    MODIFIED: app.html
    MODIFIED: bike.js

---
+++ js/bike.js
@@ -0,0 +1,48 @@
+/*
+################################################################
+#suitesofeuston.ca: bike.js
+#Copyright © 2025 Allison Munn
+#FULL COPYRIGHT NOTICE IS IN README
+################################################################
+*/
+
+function bikeSubmit(event)
+{
+  
+  var valR = true;
+  var ws = document.getElementById("FORM-BIKE-H-WS").value;
+  var msg = document.getElementById("FORM-BIKE-H-MSG").value;
+  var dateStart = document.getElementById("FORM-BIKE-DATE-START").value;
+  var dateEnd = document.getElementById("FORM-BIKE-DATE-END").value;
+  var body = ("").concat(msg, ws, "\n", "FROM:", ws, dateStart, ws, "TO:", ws, dateEnd);
+  //alert(body);
+  alert(navigator.platform);
+  
+  if(navigator.platform === "iPhone") valR = true;
+  if(navigator.platform === "MacIntel") valR = false;
+  
+  document.getElementById("FORM-BIKE-H-BODY").value = msg;
+  
+  if(dateStart > dateEnd)
+  {
+    alert("Invalid Dates");
+    valR = false;
+  }
+  
+  if(valR)
+  {
+    document.getElementById("FORM-BIKE-H-BODY").value = body;
+  }
+  else
+  {
+    event.preventDefault();
+  }
+  
+  return valR;
+}
+
+function bike()
+{
+  //var bikeForm = document.getElementById("FORM-BIKE");
+  //bikeForm.addEventListener("submit", bikeSubmit);
+}
